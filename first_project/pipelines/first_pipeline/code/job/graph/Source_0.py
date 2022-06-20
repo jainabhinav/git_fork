f85aa8f29c74dee5d365c2b60d55c8ee84d43cb8
abhinav@simpledatalabs.com <abhinav@simpledatalabs.com>
2022-06-20 07:04:00 +0000

    test2

---
+++ first_project/pipelines/first_pipeline/code/job/graph/Source_0.py
@@ -0,0 +1,8 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Source_0(spark: SparkSession) -> DataFrame:
+    return spark.read.option("header", True).option("sep", ",").csv("2")
